f0cfd439ce9cb81b9ae2bc549585451372386431
santhosh-git-23 <129048391+santhosh-git-23@users.noreply.github.com>
2023-05-17 12:05:18 +0530

    Add files via upload

---
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -0,0 +1,24 @@
+package com.example.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@SpringBootApplication
+@RestController
+public class SampleApplication {
+
+	 @GetMapping("/")
+	    public String welcomeAdmin() {
+		 return "Wecome Santhosh";
+	    }  
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+	
+	
+
+}
